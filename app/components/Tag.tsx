97e940dbd2dee07896c94174a111118a725c3c18
Rodrigo Rolim <rodrigorolimveras92@gmail.com>
2025-09-25 23:55:22 -0300

    refactor: make onClose event optional

--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -1,17 +1,17 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
-type Text = { text: string; onClose: (text: string) => void }
+type Text = { text: string; onClose?: (text: string) => void }
 export default function Tag({ text, onClose }: Text) {
   const handlerOnClose = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    onClose(text)
+    onClose?.(text)
   }
   return (
     <div className="flex text-white text-xs bg-dark-primary px-1.5 py-1 rounded-sm gap-1.5 h-fit">
       <span className="leading-4">{text}</span>
-      <button onClick={handlerOnClose}>
+      { onClose && <button onClick={handlerOnClose}>
         <XMarkIcon className="size-4" />
-      </button>
+      </button> }
     </div>
   )
 }
